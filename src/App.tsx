b7f9b8aabebd686488bb405dfe1cf8506409c4a2
Andre Capolupo <andre.capolupo@gmail.com>
2022-01-10 21:51:53 -0300

    Adicionado Style-components e font poppins do google

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
-
+      <GlobalStyle />
     </div>
   );
 }
